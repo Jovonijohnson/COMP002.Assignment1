4e95c58249e5ed298245f6715f5be53b123631ad
Jovonijohnson <Jovenom25@yahoo.com>
2024-10-20 21:57:18 -0700

    Changed script code and added main css

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-<script>
-console.log("Hello World!");
-</script>
+console.log(Hello_World); 
+
+function appendResult(text) {
+    const resultsDiv = document.getElementById('results');
+    const h2 = document.createElement('h2');
+    h2.textContent = text;
+    resultsDiv.appendChild(h2);
+}
+
+appendResult(Hello_World); 
